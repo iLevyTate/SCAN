163b38969a3e8e67c87cb72fbd3b90b04f10bcd6
Paul Sanders <paul@paulsanders.dev>
2024-05-20 11:45:20 -0400

    Dry up code

--- scan/tools/search_tools.py
+++ scan/tools/search_tools.py
@@ -12,58 +12,36 @@ class SearchTools:
     def search_internet(query):
         """Useful to search the internet
         about a given topic and return relevant results"""
-        top_result_to_return = 4
-        url = "https://google.serper.dev/search"
-        payload = json.dumps({"q": query})
-        headers = {
-            "X-API-KEY": os.getenv("SERPER_API_KEY"),
-            "content-type": "application/json",
-        }
-        response = requests.request("POST", url, headers=headers, data=payload)
-        results = response.json()["organic"]
-        string = []
-        for result in results[:top_result_to_return]:
-            try:
-                string.append(
-                    "\n".join(
-                        [
-                            f"Title: {result['title']}",
-                            f"Link: {result['link']}",
-                            f"Snippet: {result['snippet']}",
-                            "\n-----------------",
-                        ]
-                    )
-                )
-            except KeyError:
-                pass
-        return "\n".join(string)
+        return _process_results(query, url="https://google.serper.dev/search")
 
     @tool("Search news on the internet")
     def search_news(query):
         """Useful to search news about a company, stock or any other
         topic and return relevant results"""
-        top_result_to_return = 4
-        url = "https://google.serper.dev/news"
-        payload = json.dumps({"q": query})
-        headers = {
-            "X-API-KEY": os.getenv("SERPER_API_KEY"),
-            "content-type": "application/json",
-        }
-        response = requests.request("POST", url, headers=headers, data=payload)
-        results = response.json()["news"]
-        string = []
-        for result in results[:top_result_to_return]:
-            try:
-                string.append(
-                    "\n".join(
-                        [
-                            f"Title: {result['title']}",
-                            f"Link: {result['link']}",
-                            f"Snippet: {result['snippet']}",
-                            "\n-----------------",
-                        ]
-                    )
+        return _process_results(query, url="https://google.serper.dev/news")
+
+
+def _process_results(query, url, top_result_to_return=4):
+    payload = json.dumps({"q": query})
+    headers = {
+        "X-API-KEY": os.getenv("SERPER_API_KEY"),
+        "content-type": "application/json",
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    results = response.json()["organic"]
+    string = []
+    for result in results[:top_result_to_return]:
+        try:
+            string.append(
+                "\n".join(
+                    [
+                        f"Title: {result['title']}",
+                        f"Link: {result['link']}",
+                        f"Snippet: {result['snippet']}",
+                        "\n-----------------",
+                    ]
                 )
-            except KeyError:
-                pass
-        return "\n".join(string)
+            )
+        except KeyError:
+            pass
+    return "\n".join(string)
